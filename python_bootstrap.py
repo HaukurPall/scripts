1c3b63fc1eceb70bb876816b7302c0cd1402c2a6
HaukurPall <haukurpalljonsson@gmail.com>
2017-05-20 22:04:43 +0200

    more things in argparser

--- python_bootstrap.py
+++ python_bootstrap.py
@@ -3,7 +3,10 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='What_does_the_program_do?')
-    parser.add_argument('--foo', help='foo of the %(prog)s program')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-v', '--verbose', action='store_true')
+    group.add_argument('-q', '--quiet', action='store_true')
+    parser.add_argument('-f', '--foo', help='foo of the %(prog)s program')
     args = parser.parse_args()
 
 if __name__ == "__main__":
